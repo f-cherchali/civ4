59c24adfec17d62fe1cb00f14a1543b25eccb766
Fethi Cherchali <fethi.cherchali@gmail.com>
2020-08-01 19:03:12 +0100

    Defaut CIV4 basic with Admin LTE

---
+++ app/Controllers/Admin/Login.php
@@ -0,0 +1,10 @@
+<?php namespace App\Controllers\Admin;
+use \App\Controllers\BaseController;
+
+class Login extends BaseController{
+    public function index(){
+        echo view("admin/login");
+        
+    }
+}
+?>
