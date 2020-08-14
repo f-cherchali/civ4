c91957c6d8f32d50dda50b03faf9f674ef9b3e9e
Fethi Cherchali <fethi.cherchali@gmail.com>
2020-08-14 18:13:52 +0100

    update template menu

--- app/Views/admin/layout/template_head.php
+++ app/Views/admin/layout/template_head.php
@@ -161,7 +161,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview">
+          <!-- <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
@@ -189,13 +189,12 @@
                 </a>
               </li>
             </ul>
-          </li>
+          </li> -->
           <li class="nav-item">
-            <a href="../widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+            <a href="<?=site_url("dashboard")?>" class="nav-link">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Widgets
-                <span class="right badge badge-danger">New</span>
+                Tableau de bord
               </p>
             </a>
           </li>
